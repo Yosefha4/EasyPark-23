d64033b5396f706fa1182067fea58300f9ece133
Yosefha4 <73281960+Yosefha4@users.noreply.github.com>
2023-05-04 15:49:04 +0300

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -10,7 +10,7 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB1gr5GOBC-KKAHUqpZlPhVYNprk3BlOr4",
+  apiKey: "****",
   authDomain: "easypark-react-native.firebaseapp.com",
   databaseURL: "https://easypark-react-native-default-rtdb.firebaseio.com",
   projectId: "easypark-react-native",
